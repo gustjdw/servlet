b8e7e516b053c15ffce3681717b0c68e3dc8f56e
gustjdw <johs710@gachon.ac.kr>
2023-01-10 17:49:45 +0900

    유연한 컨트롤러1 - v5

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -28,5 +28,6 @@ public class MyView {
 
     private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
         model.forEach((key, value) -> request.setAttribute(key, value));
+        // model.forEach(request::setAttribute);  // lambda
     }
 }
